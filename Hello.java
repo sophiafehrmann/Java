92e147b0fba43292d59aafa42088932d2084bc8d
sophiafehrmann <54373115+sophiafehrmann@users.noreply.github.com>
2019-08-21 14:13:01 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/********************************
+* Java Program Name - Hello
+********************************
+* Lab 1 - introduction to Java
+* ______________________________
+* Sophia Fehrmann
+* 8-21-2019
+* CMSC255 - Section 002
+********************************/
+
+public class Hello {
+	public static void printHeading() {
+	System.out.println("Sophia Fehrmann");
+	System.out.println("Java Lab 1");
+	System.out.println("Introduction to Java");
+	System.out.println("August 21 2019");
+	System.out.println("CMSC255 Section 002");
+	System.out.println();
+}
+
+public static void main(String [] args) {
+	printHeading();
+	System.out.println("Hello");
+	}
+}
